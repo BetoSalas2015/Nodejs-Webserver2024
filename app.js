8c45268e216d38c4f70a50a57d5092fdfd70b373
Roberto Salazar Márquez <roberto.salazar@upaep.mx>
2024-02-18 01:21:36 -0600

    Versión 0.3.0

--- app.js
+++ app.js
@@ -1,22 +1,43 @@
 const express = require('express');
+const hbs = require('hbs');
 const app = express();
 const port = 8080;
 
+//  Handlebars
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
 app.use(express.static('public'));
 
-app.get('/index', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html')
+//  Controlador
+app.get('/', function (req, res) {
+  res.render('home', {
+    nombre: 'Roberto Salazar',
+    titulo: 'curso de node'
+  });
 });
 
-app.get('/elements', (req, res) => {
-    res.sendFile(__dirname + '/public/elements.html')
+app.get('/elements', function (req, res) {
+  res.render('elements.hbs', {
+    nombre: 'Roberto Salazar',
+    titulo: 'curso de node'
+  });
 });
 
-
-app.get('*', (req, res) => {
-    res.sendFile(__dirname + '/public/404.html');
+app.get('/generic', function (req, res) {
+  res.render('generic.hbs', {
+    nombre: 'Roberto Salazar',
+    titulo: 'curso de node'
+  });
 });
 
-app.listen(port,  () => {
+app.get('/elements.html', function (req, res) {
+  res.render('elements.hbs', {
+    nombre: 'Roberto Salazar',
+    titulo: 'curso de node'
+  });
+});
+
+app.listen(port,  () => {   
     console.log(`Aplicación de ejemplo: Escuchando el puerto http://localhost:${port}`);
 });
